c9da6fe3a3486eeefc14c10fd893cdcc544c211b
[NODERS]TEAM <94483941+nodersteam@users.noreply.github.com>
2022-05-30 21:00:44 +0300

    Update start.sh

--- tgbotjail/start.sh
+++ tgbotjail/start.sh
@@ -14,8 +14,6 @@ if [[ ${JAIL} == *"false"* ]]; then
     MSG="Project $NODENAME $MSG"
     echo -e "${JAIL} \n"
     echo -e $( echo "${PWDDD}" | ${PROJECT} tx slashing unjail --chain-id ${CHAIN_ID} --from ${WALLETNAME} --gas=auto --fees=1000${DENOM} -y) \n;
-    sleep 1
-    
     SEND=$(curl -s -X POST -H "Content-Type:multipart/form-data" "https://api.telegram.org/bot$TG_API/sendMessage?chat_id=$TG_ID&text=$MSG"); exit 1
                   
 fi
