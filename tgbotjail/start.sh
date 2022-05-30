93d6ece033fad235660c8295c3d2eaba94b12719
[NODERS]TEAM <94483941+nodersteam@users.noreply.github.com>
2022-05-30 18:24:33 +0300

    Update start.sh

--- tgbotjail/start.sh
+++ tgbotjail/start.sh
@@ -12,9 +12,10 @@ if [[ ${JAIL} == *"false"* ]]; then
     
     MSG="Warning! Server with $ip. Validator in JAIL!"
     MSG="Project $NODENAME $MSG"
-    SEND=$(curl -s -X POST -H "Content-Type:multipart/form-data" "https://api.telegram.org/bot$TG_API/sendMessage?chat_id=$TG_ID&text=$MSG"); exit 1
+    echo -e "${JAIL} \n"
+    echo -e $( echo "${PWDDD}" | ${PROJECT} tx slashing unjail --chain-id ${CHAIN_ID} --from ${WALLETNAME} --gas=auto --fees=1000${DENOM} -y) \n;
+    sleep 1
     
-                echo -e "${JAIL} \n"
-                echo -e $( echo "${PWDDD}" | ${PROJECT} tx slashing unjail --chain-id ${CHAIN_ID} --from ${WALLETNAME} --gas=auto --fees=1000${DENOM} -y) \n;
-                sleep 1
+    SEND=$(curl -s -X POST -H "Content-Type:multipart/form-data" "https://api.telegram.org/bot$TG_API/sendMessage?chat_id=$TG_ID&text=$MSG"); exit 1
+                  
 fi
