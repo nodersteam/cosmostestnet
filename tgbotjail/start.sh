d51be283909538a7057bb947a2101e7f9be65e89
[NODERS]TEAM <94483941+nodersteam@users.noreply.github.com>
2022-05-30 18:23:36 +0300

    Update start.sh

--- tgbotjail/start.sh
+++ tgbotjail/start.sh
@@ -7,10 +7,13 @@ source $HOME/.bash_profile
 JAIL=$(${PROJECT} q staking validator $( echo "${PWDDD}" | ${PROJECT} keys show ${WALLETNAME} --bech val -a) | grep jailed:);
 if [[ ${JAIL} == *"false"* ]]; then
     echo -e "${JAIL} \n"
+   
+    else
+    
     MSG="Warning! Server with $ip. Validator in JAIL!"
     MSG="Project $NODENAME $MSG"
     SEND=$(curl -s -X POST -H "Content-Type:multipart/form-data" "https://api.telegram.org/bot$TG_API/sendMessage?chat_id=$TG_ID&text=$MSG"); exit 1
-    else
+    
                 echo -e "${JAIL} \n"
                 echo -e $( echo "${PWDDD}" | ${PROJECT} tx slashing unjail --chain-id ${CHAIN_ID} --from ${WALLETNAME} --gas=auto --fees=1000${DENOM} -y) \n;
                 sleep 1
