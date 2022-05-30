59ad8c492f5d941e39abb8e3a7e434ece0cfa2cf
[NODERS]TEAM <94483941+nodersteam@users.noreply.github.com>
2022-05-30 18:01:41 +0300

    Update start.sh

--- tgbotjail/start.sh
+++ tgbotjail/start.sh
@@ -1,60 +1,17 @@
 #!/bin/bash
 
-
-LOG_FILE="/root/tgbot/nodealerts.log"
-
-NODE_RPC="http://127.0.0.1:26657"
-source 
-
-SIDE_RPC="http://localhost:26657"
 ip=$(wget -qO- eth0.me)
 
-touch $LOG_FILE
-REAL_BLOCK=$(curl -s "$SIDE_RPC/status" | jq '.result.sync_info.latest_block_height' | xargs )
-STATUS=$(curl -s "$NODE_RPC/status")
-CATCHING_UP=$(echo $STATUS | jq '.result.sync_info.catching_up')
-LATEST_BLOCK=$(echo $STATUS | jq '.result.sync_info.latest_block_height' | xargs )
-VOTING_POWER=$(echo $STATUS | jq '.result.validator_info.voting_power' | xargs )
-ADDRESS=$(echo $STATUS | jq '.result.validator_info.address' | xargs )
-source $LOG_FILE
-
-echo 'LAST_BLOCK="'"$LATEST_BLOCK"'"' > $LOG_FILE
-echo 'LAST_POWER="'"$VOTING_POWER"'"' >> $LOG_FILE
-
 source $HOME/.bash_profile
-curl -s "$NODE_RPC/status"> /dev/null
-if [[ $? -ne 0 ]]; then
-    MSG="Warning! Server with $ip. Node is stopped!"
+
+JAIL=$(${PROJECT} q staking validator $( echo "${PWDDD}" | ${PROJECT} keys show ${WALLETNAME} --bech val -a) | grep jailed:);
+if [[ ${JAIL} == *"false"* ]]; then
+    echo -e "${JAIL} \n"
+    MSG="Warning! Server with $ip. Validator in JAIL!"
     MSG="Project $NODENAME $MSG"
     SEND=$(curl -s -X POST -H "Content-Type:multipart/form-data" "https://api.telegram.org/bot$TG_API/sendMessage?chat_id=$TG_ID&text=$MSG"); exit 1
-fi
-
-if [[ $LAST_POWER -ne $VOTING_POWER ]]; then
-    DIFF=$(($VOTING_POWER - $LAST_POWER))
-    if [[ $DIFF -gt 0 ]]; then
-        DIFF="%2B$DIFF"
-    fi
-    MSG="Attention! Server with $ip. Voting power changed on $DIFF%0A($LAST_POWER -> $VOTING_POWER)"
-fi
-
-if [[ $LAST_BLOCK -ge $LATEST_BLOCK ]]; then
-
-    MSG="Attention! Server with $ip\ Node is probably stuck at block >> $LATEST_BLOCK"
-fi
-
-if [[ $VOTING_POWER -lt 1 ]]; then
-    MSG="Attention! Server with $ip validator inactive\jailed. Voting power $VOTING_POWER"
-fi
-
-if [[ $CATCHING_UP = "true" ]]; then
-    MSG="Attention! Server with $ip node is unsync, catching up. $LATEST_BLOCK -> $REAL_BLOCK"
-fi
-
-if [[ $REAL_BLOCK -eq 0 ]]; then
-    MSG="Attention! Server with $ip can't connect to >> $SIDE_RPC"
-fi
-
-if [[ $MSG != "" ]]; then
-    MSG="$NODENAME $MSG"
-    SEND=$(curl -s -X POST -H "Content-Type:multipart/form-data" "https://api.telegram.org/bot$TG_API/sendMessage?chat_id=$TG_ID&text=$MSG")
+    else
+                echo -e "${JAIL} \n"
+                echo -e $( echo "${PWDDD}" | ${PROJECT} tx slashing unjail --chain-id ${CHAIN_ID} --from ${WALLETNAME} --gas=auto --fees=1000${DENOM} -y) \n;
+                sleep 1
 fi
